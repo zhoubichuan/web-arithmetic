66efc2e81778c18fc8d35c8d00baa70b6c190898
zhoubichuan <zhoubichuan@icloud.com>
2021-09-03 12:37:46 +0800

    master

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   // 基础配置
-  base: '/Web-Arithmetic/', // 部署站点的基础路径
+  base: '/web-arithmetic/', // 部署站点的基础路径
   locales: {
     // 默认标题
     '/': {
-      title: '算法专项总结',
+      title: '算法学习笔记',
       description: ''
     }
   },
@@ -95,108 +95,30 @@ module.exports = {
         editLinkText: '在github上编辑此页',
         lastUpdated: '上次更新',
         nav: [{
-            text: '算法基础',
-            items: [{
-                text: '一、基础算法',
-                link: '/base/1.sortAlgorithm',
-              },
-              {
-                text: '二、贪心算法',
-                link: '/base/2.greedyAlgorithm',
-              }
-            ],
+          text: '算法基础',
+          items: [{
+            text: '一.基础算法',
+            link: '/base/1.sortAlgorithm',
           },
           {
-            text: '算法书籍',
-            items: [
-              {
-              text: '小灰的算法之旅',
-              link: '/books/1.xiaohuiAlgorithmTour',
-              },
-              {
-                text: '学习Javascript数据结构与算法',
-                link: '/books/1.xiaohuiAlgorithmTour',
-              },
-            ]
-          },
-          {
-            text: 'leetCode',
-            items: [{
-              text: '一、Node',
-              link: '/service/node',
-            }, {
-              text: '二、Express',
-              link: '/service/express',
-            }, {
-              text: '三、Koa',
-              link: '/service/koa',
-            }, {
-              text: '四、Egg',
-              link: '/service/egg',
-            }]
-          },
-          {
-            text: '大厂面试题',
-            link: '/article/',
-            items: [{
-              text: '阿里巴巴',
-              link: '/article/阿里巴巴',
-            }, {
-              text: '饿了么',
-              link: '/article/饿了么',
-            }, {
-              text: '百分点',
-              link: '/article/百分点',
-            }, {
-              text: '宝宝树',
-              link: '/article/宝宝树',
-            }, {
-              text: '滴滴',
-              link: '/article/滴滴',
-            }, {
-              text: '兑吧',
-              link: '/article/兑吧',
-            }, {
-              text: '海风教育',
-              link: '/article/宝海风教育',
-            }, {
-              text: '海康威视',
-              link: '/article/海康威视',
-            }, {
-              text: '沪江',
-              link: '/article/沪江',
-            }, {
-              text: '今日头条',
-              link: '/article/今日头条',
-            }, {
-              text: '酷家乐',
-              link: '/article/酷家乐',
-            }, {
-              text: '蘑菇街',
-              link: '/article/蘑菇街',
-            }, {
-              text: '寺库',
-              link: '/article/寺库',
-            }, {
-              text: '挖财',
-              link: '/article/挖财',
-            }, {
-              text: '网易',
-              link: '/article/网易',
-            }, {
-              text: '微医',
-              link: '/article/微医',
-            }, {
-              text: '喜马拉雅',
-              link: '/article/喜马拉雅',
-            }, {
-              text: '携程',
-              link: '/article/携程',
-            }, {
-              text: '有赞',
-              link: '/article/有赞',
-            }]
+            text: '二.贪心算法',
+            link: '/base/2.greedyAlgorithm',
           }
+          ],
+        },
+        {
+          text: '算法书籍',
+          items: [
+            {
+              text: '一.小灰的算法之旅',
+              link: '/books/1/1.index',
+            },
+            {
+              text: '二.学习Javascript数据结构与算法',
+              link: '/books/2/1.index',
+            },
+          ]
+        }
         ],
         sidebar: {
           '/base/': [
@@ -213,150 +135,10 @@ module.exports = {
             '11.linkedListCorrelationAlgorithm',
             '12.treeCorrelationAlgorithm'
           ],
-          '/books/': ['1.xiaohuiAlgorithmTour', 'React'],
-          '/service/': [{
-              title: '一、node',
-              collapsable: false,
-              children: ['node']
-            },
-            {
-              title: '二、express',
-              collapsable: false,
-              children: ['express']
-            },
-            {
-              title: '三、koa',
-              collapsable: false,
-              children: ['koa']
-            },
-            {
-              title: '四、egg',
-              collapsable: false,
-              children: ['egg']
-            }
-          ],
-          '/article/': [{
-              title: '阿里相关',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '阿里巴巴',
-                '蘑菇街',
-                '饿了么'
-              ]
-            },
-            {
-              title: '今日头条',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '今日头条',
-              ]
-            },
-            {
-              title: '滴滴',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '滴滴',
-              ]
-            },
-            {
-              title: '兑吧',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '兑吧',
-              ]
-            },
-            {
-              title: '百分点',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '百分点',
-              ]
-            },
-            {
-              title: '海风教育',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '海风教育',
-              ]
-            },
-            {
-              title: '海康威视',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '海康威视',
-              ]
-            },
-            {
-              title: '沪江',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '沪江',
-              ]
-            },
-            {
-              title: '酷家乐',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '酷家乐',
-              ]
-            },
-            {
-              title: '寺库',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '寺库',
-              ]
-            },
-            {
-              title: '今日头条',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '今日头条',
-              ]
-            },
-            {
-              title: '挖财',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '挖财',
-              ]
-            },
-            {
-              title: '网易',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '网易',
-              ]
-            },
-            {
-              title: '微医',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '微医',
-              ]
-            },
-            {
-              title: '携程',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '携程',
-              ]
-            },
-            {
-              title: '喜马拉雅',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '喜马拉雅',
-              ]
-            },
-            {
-              title: '有赞',
-              collapsable: false, // 让一个组永远都是展开状态
-              children: [
-                '有赞',
-              ]
-            },
-          ]
+          '/books/1': ['1.index'],
+          '/books/2': ['1.index'],
+          '/books/3': ['1.index'],
+          '/books/4': ['1.index'],
         }
       }
     }
